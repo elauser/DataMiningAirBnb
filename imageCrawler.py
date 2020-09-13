2ac8d6b40129abb6949ab42404fefcd470b0dc3b
elauser <1810653024@stud.fh-kufstein.ac.at>
2020-09-13 10:13:14 +0900

    cleanup

--- imageCrawler.py
+++
@@ -1,8 +0,0 @@
-import csv
-import urllib.request
-
-with open("listingsNy.csv", encoding="utf-8") as csv_file:
-    csv_reader = csv.DictReader(csv_file, delimiter=",")
-    for row in csv_reader:
-        print(row["host_picture_url"])
-        urllib.request.urlretrieve(row["host_picture_url"], "Images/" + row["id"] + ".jpg")
